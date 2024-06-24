4e1ec98922829465c61b9031efcda68a63ed1a02
Aayushi Parekh <aayushiparekh2812@gmail.com>
2024-06-24 16:48:31 -0400

    form update

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,20 +1,37 @@
+import { DatePipe } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatListModule, DatePipe],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TasksComponent {
-  datePicker!: FormControl;
   priority : any = ['High', 'Medium', 'Low'];
+  tasks: any = [
+    {taskName: 'sax', deadlineDate: 'Tue Jun 11 2024 00:00:00 GMT-0400 (Eastern Daylight Time)', prioritySelected: 'Medium'}]
   selectedValue: any;
+  animalControl = new FormControl(null, Validators.required);
+  
+  taskForm = new FormGroup({
+    taskName: new FormControl('', Validators.required),
+    deadlineDate: new FormControl('', Validators.required),
+    prioritySelected: new FormControl('', Validators.required),
+  })
+
+  onSubmit(){
+    console.log(this.taskForm.value);
+    this.tasks.push(this.taskForm.value);
+    console.log(this.tasks);
+    this.taskForm.reset();
+  }
 }
